7d9523e5d6dcfe90b411a48487afccd63fa29d34
Akshay4026 <vadlamani.akshaykumar@gmail.com>
2025-11-10 03:07:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def execute_code(file_path):
 def get_youtube_comments(video_id, max_results):
     api_service_name = "youtube"
     api_version = "v3"
-    DEVELOPER_KEY = "AIzaSyD1x6zgv9mkD68QGPIub2mJEvD_MY4dTHk"  # Replace with your own API key
+    DEVELOPER_KEY = "API_KEY"
 
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, developerKey=DEVELOPER_KEY)
